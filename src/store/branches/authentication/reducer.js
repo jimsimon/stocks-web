dfc0fd048d9acd3c7508977d5a35c291829ed958
Jim Simon <jim.j.simon@gmail.com>
2018-07-28 03:12:09 -0400

    Store token in session storage for reuse across page reloads

--- src/store/branches/authentication/reducer.js
+++ src/store/branches/authentication/reducer.js
@@ -1,17 +1,24 @@
 import {SET_TOKEN} from './action-types'
 
-export default function reducer (state = {}, {type, payload, error}) {
+export default function reducer (state = initialize(), {type, payload, error}) {
   if (error) {
     return state
   }
 
   switch (type) {
     case SET_TOKEN:
+      sessionStorage.setItem('token', payload)
       return Object.assign({}, state, {
         token: payload
       })
 
     default:
       return state
+  }
+}
+
+function initialize () {
+  return {
+    token: sessionStorage.getItem('token')
   }
 }
